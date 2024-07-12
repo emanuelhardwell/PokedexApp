38710dc3962aaa5c1031319660709e956d9cb599
emanuelhardwell <emanuelnoe16@gmail.com>
2024-07-11 22:28:17 -0600

    feat: Update actions getPokemons and create barrel file interfaces

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,18 +1,31 @@
 import {pokeApi} from '../../config/api/pokeApi';
 import {Pokemon} from '../../domain/entities/Pokemon';
+import type {
+  PokeapiPaginated,
+  PokeapiPokemon,
+} from '../../infrastructure/interfaces';
 
 // To fake a slow connection
 export const sleep = async () => {
   return new Promise((resolve, reject) => setTimeout(resolve, 2000));
 };
 
-export const getPokemons = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (
+  page: number,
+  limit: number = 10,
+): Promise<Pokemon[]> => {
   //   await sleep();
 
   try {
-    const url = '/pokemon';
-    const {data} = await pokeApi.get(url);
-    console.log(data);
+    const url = `/pokemon?offset=${page * 10}&limit=${limit}`;
+    const {data} = await pokeApi.get<PokeapiPaginated>(url);
+
+    const pokemonsPromises = data.results.map(pokemon => {
+      return pokeApi.get<PokeapiPokemon>(pokemon.url);
+    });
+
+    const pokemonsResult = await Promise.all(pokemonsPromises);
+    console.log(pokemonsResult[0]);
 
     return [];
   } catch (error) {
